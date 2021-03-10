e1dd0c923b852102ca5b5b2e8deac80190968e28
FlyingPlum <lixiangyu19970123@hotmail.com>
2021-03-11 02:16:10 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+
+namespace nwneesaveeditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+    //Define Feat
+    public class Feat
+    {
+        public Feat() { }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    //Define Spell
+    public class Spell
+    {
+        public Spell() { }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        //level flags
+        public enum Level
+        {
+            Level0 = 1,
+            Level1 = 2,
+            Level2 = 4,
+            Level3 = 8,
+            Level4 = 16,
+            Level5 = 32,
+            Level6 = 64,
+            Level7 = 128,
+            Level8 = 256,
+            Level9 = 512,
+        }
+        //class flags
+        public enum Class
+        {
+            wizard = 1,
+            cleric = 2,
+            druid = 4,
+            sorcer = 8,
+            paladin = 16,
+            ranger = 32,
+            poet = 64,
+        }
+    }
+
+    //json setting file decoding
+    //open bic
+    //transformation bic->json
+    //transformation json->bic
+    //edit json - add/remove feat
+    //edit json - add/remove spell
+    //save bic
+    //close bic
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace nwneesaveeditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //localization
+            this.Title = (string)Application.Current.FindResource("MainWindowTitle");
+            this.filemenu.Header = (string)Application.Current.FindResource("FileMenuHeader");
+            this.helpmenu.Header = (string)Application.Current.FindResource("HelpMenuHeader");
+            this.openfile.Icon = new System.Windows.Controls.Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/OpenFile_16x.png"))
+            };
+            this.openfile.Header = (string)Application.Current.FindResource("OpenFileHeader");
+            this.closefile.Icon = new System.Windows.Controls.Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/CloseDocument_16x.png"))
+            };
+            this.closefile.Header = (string)Application.Current.FindResource("CloseFileHeader");
+            this.savefile.Icon = new System.Windows.Controls.Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/Save_16x.png"))
+            };
+            this.savefile.Header = (string)Application.Current.FindResource("SaveFileHeader");
+            this.saveasfile.Icon = new System.Windows.Controls.Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/SaveAs_16x.png"))
+            };
+            this.saveasfile.Header = (string)Application.Current.FindResource("SaveAsFileHeader");
+            this.lang.Icon = new System.Windows.Controls.Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/LanguagePackageApplication_16x.png"))
+            };
+            this.lang.Header = (string)Application.Current.FindResource("LangHeader");
+            this.about.Header = (string)Application.Current.FindResource("AboutHeader");
+            this.feattab.Header = (string)Application.Current.FindResource("FeatTabHeader");
+            this.spelltab.Header = (string)Application.Current.FindResource("SpellTabHeader");
+            //add list and button
+            //add message box for further information
+            //add filter
+            //click event handle
+        }
+    }
+}
